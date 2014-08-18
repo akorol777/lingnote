9d16511e76a0127cf69f53bf5c1112c6c1ddd06c
akorol777 <akorol777@ya.ru>
2014-08-18 23:40:26 +0300

    respons test

--- js/common.js
+++ js/common.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
+
 	$('.header__btn').click(function(){
 		$('.main').addClass('main-open');
+		$('.nav').addClass('nav-open');
 		return false;
 	});
 	$(this).click(function(){
 		$('.main').removeClass('main-open');
+		$('.nav').removeClass('nav-open');
 		return false;
 	});	
+
+	$('.user__pic').click(function(){
+		$('.user').toggleClass('user-open');
+		return false;
+	});
+	$(document).click(function(){
+		$('.user').removeClass('user-open');
+		return false;
+	});	
+
 });
 
 
