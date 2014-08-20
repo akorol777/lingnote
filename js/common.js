77a7c480ae10554df738a641cc9c55276a326c46
akorol777 <akorol777@ya.ru>
2014-08-20 23:16:29 +0300

    fix 200814

--- js/common.js
+++ js/common.js
@@ -7,19 +7,48 @@ $(document).ready(function() {
 	});
 	$(this).click(function(){
 		$('.user').removeClass('user-open');
+		$('.nav__item-popup').removeClass('popup-open');
+		$('.nav__item-lose').removeClass('lose-open');				
 		return false;
 	});
+
+	$('.nav__item-point').click(function(){
+		$('.nav__item-popup').removeClass('popup-open');
+		$('.nav__item-lose').removeClass('lose-open');		
+		$(this).next().toggleClass('popup-open');
+		return false;
+	});
+	$('.nav__item').click(function(){
+		$('.nav__item-popup').removeClass('popup-open');	
+		$('.nav__item-lose').removeClass('lose-open');			
+		return false;
+	});	
+
+	$('.nav__item-link').click(function(){
+		$(this).parent().parent().find('.nav__item-lose').addClass('lose-open');
+		return false;
+	});
+
+
 
 	$('.content, .header').click(function(){
 		$('.nav').removeClass('nav-open');
 		$('.main').removeClass('main-open');
-		$('.user').removeClass('user-open');		
+		$('.user').removeClass('user-open');
+		$('.nav__item-popup').removeClass('popup-open');
+		$('.nav__item-lose').removeClass('lose-open');						
 		return false;
 	});	
 
-	$('.nav__content, .nav__topper, .user').click(function(){
+	$('.nav__content, .nav__topper, .user, .nav__item-popup').click(function(){
 		return false;
 	});	
+
+	$('.nav__topper').click(function(){
+		$('.nav__item-popup').removeClass('popup-open');
+		$('.nav__item-lose').removeClass('lose-open');			
+		return false;
+	});		
 	
 
 	$('.user__pic').click(function(){
